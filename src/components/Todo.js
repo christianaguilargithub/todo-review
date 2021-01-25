9764fc1e86e71a85fae3a8d54b8b3d5cabc7f10f
aguilar.chrisgitlab <aguilar.chris9123@gmail.com>
2021-01-25 14:09:01 +0800

    curTodo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Todo = ({text,todo, todos,setTodos}) => {
     const deleteHandler = () => {
-        setTodos(todos.filter((el) => el.id !==todo.id))
+        setTodos(todos.filter((curTodo) => curTodo.id !==todo.id))
             };
     const completeHandler = () => {
         setTodos(todos.map(item =>{
             if(item.id === todo.id){
                      return{
-                            ...item, 
+                            ...item,
                             completed: !item.completed 
-                        }
+                           }
                     } 
                     return item;
                 }));

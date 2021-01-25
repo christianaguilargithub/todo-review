b69a08a63b08638e2367c7a3f284f690b70f4aa0
aguilar.chrisgitlab <aguilar.chris9123@gmail.com>
2021-01-25 12:50:36 +0800

    revert changes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Todo = ({text,todo, todos,setTodos}) => {
     const deleteHandler = () => {
-        setTodos(todos.filter((curTodo) => curTodo.id !==todo.id))
+        setTodos(todos.filter((el) => el.id !==todo.id))
             };
     const completeHandler = () => {
         setTodos(todos.map(item =>{

9b6808c4e21ce2990054840eac179007149377da
aguilar.chrisgitlab <aguilar.chris9123@gmail.com>
2021-01-30 01:30:43 +0800

    used Deno (web module) Import import ShortUniqueId from 'https://cdn.jsdelivr.net/npm/short-unique-id@latest/short_uuid/mod.ts'; instead of Math.random(); for unique id's and no duplication

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ShortUniqueId from 'https://cdn.jsdelivr.net/npm/short-unique-id@latest/short_uuid/mod.ts';
 
 function Form({
                 setStatus,
@@ -16,9 +17,9 @@ function Form({
                 setTodos([
                           ...todos,
                          {
+                           id: ShortUniqueId,
                            text: inputText,
                            completed: false,
-                           id: Math.random()
                          }
                     ]);
                 setInputText("");
